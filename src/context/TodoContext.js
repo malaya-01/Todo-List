9378c34838cf99054d716df74d8becd1097c46af
malaya-01 <practice.git.963@gmail.com>
2024-06-06 15:45:56 +0530

    todo list

---
+++ src/context/TodoContext.js
@@ -0,0 +1,46 @@
+import { createContext, useContext } from "react"
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo: "Todo message",
+            completed: false
+        },
+    ],
+    addTodo: (todo) => {},
+    updateTodo:(id,Todo) => {},
+    deleteTodo:(id) => {},
+    toggleComplete: (id) => {}
+})
+
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
+
+
+// import {createContext, useContext} from "react"
+
+// export const TodoContext = createContext({
+//     todos: [
+//         {
+//             id: 1,
+//             todo: " Todo msg",
+//             completed: false,
+//         }
+//     ],
+//     addTodo: (todo) => {},
+//     updateTodo: (id, todo) => {},
+//     deleteTodo: (id) => {},
+//     toggleComplete: (id) => {}
+// })
+
+
+// export const useTodo = () => {
+//     return useContext(TodoContext)
+// }
+
+// export const TodoProvider = TodoContext.Provider
